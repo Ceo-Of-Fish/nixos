88c3f2cfc03d14aba3d6ffce7c73bf67537dbff8
bananaman83 <bananaman83@bananaman83.com>
2026-09-14 16:49:49 +0000

    Add files via upload

--- configs-and-more/boot.nix
+++ configs-and-more/boot.nix
@@ -4,6 +4,7 @@
   boot.loader.limine = {
     enable = true;
     maxGenerations = 15;
+    efiInstallAsRemovable = true; # Some devices may need this to boot correctly
   };
   ## Enable the items below if using bios and comment the ones above.
 #  boot.loader.grub = {
